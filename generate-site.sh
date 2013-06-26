5eb4663a6794f228989cdb8ba6eab738bee639f3
Zach A. Thomas <zach@aeroplanesoftware.com>
2013-06-26 16:59:59 -0500

    rearrange one more time. jekyll on github doesn't want you to set source attribute.

--- generate-site.sh
+++ generate-site.sh
@@ -3,6 +3,6 @@
 VERS=`git rev-parse --short HEAD`
 DATE=`date +%Y-%m-%d`
 
-ASCIIDOC_HTML="asciidoc --backend=html5 --conf-file=layout.conf --attribute icons --attribute iconsdir=./images/icons --attribute=badges --attribute=revision=$VERS  --attribute=date=$DATE"
+ASCIIDOC_HTML="asciidoc --backend=html5 --conf-file=source/layout.conf --attribute icons --attribute iconsdir=./images/icons --attribute=badges --attribute=revision=$VERS  --attribute=date=$DATE"
 
-$ASCIIDOC_HTML -a badges -o static/index.html index.adoc
+$ASCIIDOC_HTML source/index.adoc
