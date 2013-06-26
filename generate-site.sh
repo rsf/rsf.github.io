b593195fd7082a142be9c722de83b21f5be1c898
Zach A. Thomas <zach@aeroplanesoftware.com>
2013-06-26 17:30:56 -0500

    fix the output location of the asciidoc generator. More detail for the README.

--- generate-site.sh
+++ generate-site.sh
@@ -5,4 +5,4 @@ DATE=`date +%Y-%m-%d`
 
 ASCIIDOC_HTML="asciidoc --backend=html5 --conf-file=source/layout.conf --attribute icons --attribute iconsdir=./images/icons --attribute=badges --attribute=revision=$VERS  --attribute=date=$DATE"
 
-$ASCIIDOC_HTML source/index.adoc
+$ASCIIDOC_HTML -o ./index.html source/index.adoc
